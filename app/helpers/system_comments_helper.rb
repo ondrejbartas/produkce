a374089c8d78e2fe8010126afe31fe4056f19234
Ondřej Bartas <o.bartas@gmail.com>
2009-04-21 16:44:32 +0200

    edit system comments

--- app/helpers/system_comments_helper.rb
+++ app/helpers/system_comments_helper.rb
@@ -10,22 +10,21 @@ module SystemCommentsHelper
   	xm << "'>\n"
   	xm << "<li class='name'>\n"
   	  if !system_comment.user_id.blank?
-  	     xm << system_comment.user.fullname
+  	     xm << " "+system_comment.user.fullname
   	  end
   	xm << ", "+my_date(system_comment.created_at)
   	xm << "<span class='odkazy'>"
   	if !(system_comment.finished == true || finished == true)
-  	  xm << right_link("system_comments", "new", 'odpovědět', new_system_comment_system_comment_path(system_comment.id))
+  	  xm << " "+right_link("system_comments", "new", 'odpovědět', new_system_comment_system_comment_path(system_comment.id)).to_s
   	else 
   	  finished = true
   	end
-  	xm << right_link("system_comments", "edit", 'Edit', edit_system_comment_path(system_comment))
+  	xm << " "+ right_link("system_comments", "edit", 'Edit', edit_system_comment_path(system_comment)).to_s
   	xm << "</span></li>\n"
   	xm << "<li class='text'>"+system_comment.note+"</li>\n"
     
     if system_comment.system_comments.size > 0
         for comment in system_comment.system_comments.sort_by {|u| u.created_at}
-        
           xm << system_comment_show(comment, finished)
         end
     end  
